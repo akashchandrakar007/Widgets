344a6d6ae80f010e89a07dbfed738c3582622090
Akashdeep Chandrakar <63792903+akashchandrakar@users.noreply.github.com>
2022-03-06 12:47:02 +0530

    Completed widgets

---
+++ src/components/Link.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Link=({className,href,children})=>{
+
+    const onClick=(event)=>{
+      event.preventDfault();
+      window.history.pushState({},'',href);
+
+      const navEvent=new PopStateEvent('popstate');
+      window.dispatchEvent(navEvent);
+    };
+   return (
+     <a className={className} href={href}>
+      {children}
+     </a>
+   );
+};
+export default Link;
